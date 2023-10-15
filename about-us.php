21548b4b254eb84cc1aa352a42ea55186c55501d
Badr JLIL <badr.jlil@outlook.com>
2023-10-15 15:47:22 +0100

    created about page

---
+++ about-us.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!--=============== CSS ===============-->
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/about-us.css">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
+
+	<title>Creastation - About us</title>
+
+</head>
+
+<body>
+	<!--=============== HEADER ===============-->
+	<?php
+    include 'assets/php/user_header.php';
+  ?>
+	<section class="section1" id="AboutUs">
+		<table>
+			<tr>
+				<td>
+					<img src="https://images.unsplash.com/photo-1499083097717-a156f85f0516?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
+						alt="AboutUs">
+				</td>
+				<td>
+					<span class="title">Our Story</span>
+					<span class="sub-title">Lorem ipsum dolor sit amet, consectetur adi</span>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+						voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua.</p>
+				</td>
+			</tr>
+		</table>
+	</section>
+
+	<section class="section2" id="OurMission">
+		<table>
+			<tr>
+				<td>
+					<span class="title">Our Mission</span>
+					<span class="sub-title">Lorem ipsum dolor sit amet, consectetur adi</span>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+						voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua.</p>
+				</td>
+				<td>
+					<img src="https://images.unsplash.com/photo-1499083097717-a156f85f0516?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
+						alt="AboutUs">
+				</td>
+			</tr>
+		</table>
+	</section>
+
+	<div class="section3">
+		<span class="title">Our Vision</span>
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+						voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+		
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua.</p>
+						<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+						voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+						voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+		</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua.</p>
+
+	</div>
+
+	<div class="section4">
+		<span class="title">Our Values</span>
+		<div class="testimonials">
+
+			<div class="card">
+				<div class="layer"></div>
+				<div class="content">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+							labore et dolore magna aliqua.</p>
+					<div class="image">
+						<img src="https://cdn-icons-png.flaticon.com/512/912/912278.png ">
+					</div>
+					<div class="details">
+						<h2>Innovation<br><span>Innovation fuels success</span></h2>
+					</div>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="layer"></div>
+				<div class="content">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+							labore et dolore magna aliqua.</p>
+					<div class="image">
+						<img src="https://cdn-icons-png.flaticon.com/512/6808/6808956.png">
+					</div>
+					<div class="details">
+						<h2>Collaboration<br><span>Synergy powers excellence</span></h2>
+					</div>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="layer"></div>
+				<div class="content">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+							labore et dolore magna aliqua.</p>
+					<div class="image">
+						<img src="https://cdn-icons-png.flaticon.com/512/2857/2857433.png">
+					</div>
+					<div class="details">
+						<h2>Creativity<br><span>Ethical actions, lasting impact</span></h2>
+					</div>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="layer"></div>
+				<div class="content">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+							labore et dolore magna aliqua.</p>
+					<div class="image">
+						<img src="https://cdn-icons-png.flaticon.com/512/4371/4371336.png">
+					</div>
+					<div class="details">
+						<h2>Responsibility<br><span>Fostering boundless creative potential</span></h2>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+
+	<div class="section5">
+	<span class="title">Why choose us</span>
+		<section class="wcu-container">
+			<div class="wcu-grid wcu">
+				<h5>Expertise</h5>
+				<p>Our team is not just a group of professionals; they are seasoned experts with years of hands-on experience in the realms of digital marketing, web development, and mobile app development. Their extensive knowledge and skills have been honed through successfully executing a myriad of projects, making them adept at navigating the complexities of the digital landscape. When you choose us, you're choosing a team that understands the nuances of the industry, ensuring your projects are in the hands of capable and knowledgeable professionals.</p>
+			</div>
+			<div class="wcu-grid wcu wcu2">
+				<h5>Customized Solutions</h5>
+				<p>Recognizing that every business is unique, we go the extra mile to understand your specific needs and challenges. We don't believe in one-size-fits-all solutions. Instead, we meticulously tailor our services to align with your business objectives. By analyzing your requirements in-depth, we craft bespoke strategies and applications that precisely cater to your individual demands. Our focus is on delivering personalized solutions that address your distinctive needs, ensuring maximum efficiency and impact for your business.</p>
+			</div>
+			<div class="wcu-grid wcu">
+				<h5>Results-Driven</h5>
+				<p>In a world inundated with digital strategies, we stand out by prioritizing results over rhetoric. While we appreciate creativity, we are firm believers in the power of measurable outcomes. Our approach is rooted in delivering tangible, quantifiable results for our clients. We don't just stop at conceptualizing innovative ideas; we implement them in ways that yield real, impactful results. When you partner with us, you can expect a focus on achieving your goals, whether it's increased engagement, higher conversion rates, or enhanced brand visibility.</p>
+			</div>
+			<div class="wcu-grid">
+				<h5>Client-Centric</h5>
+				<p>Your success is not just a goal; it's our guiding principle. We are committed to ensuring your satisfaction at every step of the journey. Our client-centric approach means we listen intently to your needs, communicate transparently, and actively involve you in the decision-making process. We understand the importance of collaboration and strive to build a strong partnership with you. Your feedback and insights are not just valued; they are integral to shaping our strategies and solutions. Your success story is our ultimate measure of achievement.</p>
+			</div>
+			<div class="wcu-grid wcu2">
+				<h5>Cutting-Edge Technology:</h5>
+				<p>In a digital landscape that evolves at lightning speed, staying ahead of the curve is not just an advantage; it's a necessity. We pride ourselves on our deep understanding of emerging technologies and trends. By constantly researching, learning, and adapting, we ensure that the solutions we provide are not just up-to-date but also forward-thinking. Our commitment to incorporating cutting-edge technology into our services means you receive the most innovative and effective solutions, keeping your business at the forefront of digital advancement.</p>
+			</div>
+			<div class="wcu-grid">
+				<h5>Competitive Pricing</h5>
+				<p>Quality should never be compromised by cost. We firmly believe in offering high-quality services at competitive prices. Our pricing structure is designed to be fair and transparent, ensuring you get exceptional value for your investment. We understand the budget constraints that businesses often face, and our goal is to provide top-notch services without straining your finances. With us, you can be confident that you're getting the best of both worlds: superior quality and affordability.</p>
+			</div>
+		</section>
+	</div>
+
+  <!--=============== FOOTER ===============-->
+  <?php
+    include 'assets/php/user_footer.php';
+  ?>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -22,22 +22,9 @@
 
 <body>
   <!--=============== HEADER ===============-->
-  <header class="header hidden" id="header">
-    <nav class="nav container">
-      <a href="" class="nav_logo">
-        <img src="assets/img/logo.png">
-      </a>
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About us</a></li>
-        <li><a href="#values">Our Values</a></li>
-        <li><a href="#services">Our Services</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#contact">Contact us</a></li>
-        <li><a href="#blog">Blog</a></li>
-      </ul>
-    </nav>
-  </header>
+  <?php
+    include 'assets/php/user_header.php';
+  ?>
 
   <main class="main">
     <!--=============== HOME ===============-->
@@ -71,6 +58,9 @@
           be your catalyst for success, as we combine passion, strategy, and skill to deliver solutions that inspire and
           transform.
         </div>
+        <a href="about-us.php">Read more</a>
+      </div>
+
     </section>
 
     <!--=============== OUR VALUES ===============-->
@@ -346,68 +336,9 @@
   </main>
 
   <!--=============== FOOTER ===============-->
-  <footer class="footer-distributed">
-
-    <div class="footer-left">
-
-      <h3>Creas<span>tation</span></h3>
-
-      <p class="footer-links">
-        <a href="#" class="link-1">Home</a>
-        
-        <a href="#">About us</a>
-      
-        <a href="#">Our Values</a>
-      
-        <a href="#">Our Services</a>
-        
-        <a href="#">Portfolio</a>
-        
-        <a href="#">Contact Us</a>
-      </p>
-
-      <p class="footer-company-name">All rights reserved ©2015</p>
-    </div>
-
-    <div class="footer-center">
-
-      <div>
-        <i class="fa fa-map-marker"></i>
-        <p><span>34 Rue 17 Ennasr, Lt 103</span> Tangier, Maroc</p>
-      </div>
-
-      <div>
-        <i class="fa fa-phone"></i>
-        <p>+212 512-345678</p>
-      </div>
-
-      <div>
-        <i class="fa fa-envelope"></i>
-        <p><a href="mailto:support@creastation.ma">support@creastation.ma</a></p>
-      </div>
-
-    </div>
-
-    <div class="footer-right">
-
-      <p class="footer-company-about">
-        <span>About the company</span>
-        Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
-      </p>
-
-      <div class="footer-icons">
-
-        <a href="#"><i class="fa-brands fa-facebook"></i></i></a>
-        <a href="#"><i class="fa-brands fa-instagram"></i></i></a>
-        <a href="#"><i class="fa-brands fa-x-twitter"></i></i></a>
-        <a href="#"><i class="fa-brands fa-linkedin"></i></i></a>
-
-
-      </div>
-
-    </div>
-
-  </footer>
+  <?php
+    include 'assets/php/user_footer.php';
+  ?>
 
   
 

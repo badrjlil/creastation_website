0cabe4cb867bfada325b6bdec867e0d1b11d5ac1
Badr JLIL <badr.jlil@outlook.com>
2023-10-15 15:46:56 +0100

    separated header & footer

---
+++ assets/php/user_footer.php
@@ -0,0 +1,62 @@
+<footer class="footer-distributed">
+
+    <div class="footer-left">
+
+      <h3>Creas<span>tation</span></h3>
+
+      <p class="footer-links">
+        <a href="#" class="link-1">Home</a>
+        
+        <a href="#">About us</a>
+      
+        <a href="#">Our Values</a>
+      
+        <a href="#">Our Services</a>
+        
+        <a href="#">Portfolio</a>
+        
+        <a href="#">Contact Us</a>
+      </p>
+
+      <p class="footer-company-name">All rights reserved ©2015</p>
+    </div>
+
+    <div class="footer-center">
+
+      <div>
+        <i class="fa fa-map-marker"></i>
+        <p><span>34 Rue 17 Ennasr, Lt 103</span> Tangier, Maroc</p>
+      </div>
+
+      <div>
+        <i class="fa fa-phone"></i>
+        <p>+212 512-345678</p>
+      </div>
+
+      <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="mailto:support@creastation.ma">support@creastation.ma</a></p>
+      </div>
+
+    </div>
+
+    <div class="footer-right">
+
+      <p class="footer-company-about">
+        <span>About the company</span>
+        Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+      </p>
+
+      <div class="footer-icons">
+
+        <a href="#"><i class="fa-brands fa-facebook"></i></i></a>
+        <a href="#"><i class="fa-brands fa-instagram"></i></i></a>
+        <a href="#"><i class="fa-brands fa-x-twitter"></i></i></a>
+        <a href="#"><i class="fa-brands fa-linkedin"></i></i></a>
+
+
+      </div>
+
+    </div>
+
+  </footer>
---
+++ assets/php/user_header.php
@@ -0,0 +1,16 @@
+<header class="header hidden" id="header">
+    <nav class="nav container">
+      <a href="" class="nav_logo">
+        <img src="assets/img/logo.png">
+      </a>
+      <ul>
+        <li><a href="index.php#home">Home</a></li>
+        <li><a href="index.php#about">About us</a></li>
+        <li><a href="index.php#values">Our Values</a></li>
+        <li><a href="index.php#services">Our Services</a></li>
+        <li><a href="index.php#portfolio">Portfolio</a></li>
+        <li><a href="index.php#contact">Contact us</a></li>
+        <li><a href="index.php#blog">Blog</a></li>
+      </ul>
+    </nav>
+  </header>
